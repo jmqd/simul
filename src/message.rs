e91ed043b4887e77f2e1c4a66dfed9247cb2f01a
Jordan McQueen <j@jm.dev>
2023-07-05 20:59:36 +0900

    Improve doc comments for Message

--- src/message.rs
+++ src/message.rs
@@ -1,10 +1,13 @@
-/// A message is an interaction between Agents w/ the necessary data to perform
-/// the interaction.
+/// A Message represents an interaction between Agents.
 #[derive(Default, Debug, Clone)]
 pub struct Message {
+    /// When the message was first created and put onto a queue.
     pub queued_time: u64,
+    /// When the Message was consumed and processed.
     pub completed_time: Option<u64>,
+    /// The name of the Agent that created this Message.
     pub source: String,
+    /// The name of the Agent that received this Message.
     pub destination: String,
 }
 
