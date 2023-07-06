127c68bf8ba811bf88a680f9fa261451dae1f003
Jordan McQueen <j@jm.dev>
2023-07-06 14:32:50 +0900

    Use DiscreteTime in message.rs

--- src/message.rs
+++ src/message.rs
@@ -1,10 +1,12 @@
+use crate::DiscreteTime;
+
 /// A Message represents an interaction between Agents.
 #[derive(Default, Debug, Clone)]
 pub struct Message {
     /// When the message was first created and put onto a queue.
-    pub queued_time: u64,
+    pub queued_time: DiscreteTime,
     /// When the Message was consumed and processed.
-    pub completed_time: Option<u64>,
+    pub completed_time: Option<DiscreteTime>,
     /// The name of the Agent that created this Message.
     pub source: String,
     /// The name of the Agent that received this Message.
@@ -12,7 +14,7 @@ pub struct Message {
 }
 
 impl Message {
-    pub fn new<S>(time: u64, src: S, dst: S) -> Message
+    pub fn new<S>(time: DiscreteTime, src: S, dst: S) -> Message
     where
         S: Into<String>,
     {
