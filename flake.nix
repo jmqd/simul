bb22cc97ac51b91b16a0b718a9c3fa091be6e3a1
Jordan McQueen <j@jm.dev>
2025-11-22 17:10:34 +0900

    Use rust-overlay for flake.nix

--- flake.nix
+++ flake.nix
@@ -1,3 +1,33 @@
-{ pkgs ? import <nixpkgs> { } }:
+{
+  description = "Rust dev environment";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-pkgs.mkShell { buildInputs = [ pkgs.openssl pkgs.pkg-config ]; }
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rustToolchain =
+          pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            rustToolchain
+            openssl
+            cargo-deny
+            cargo-edit
+            cargo-watch
+            cargo-udeps
+          ];
+
+          shellHook = ''
+            # Tells rust-analyzer where the stdlib sources are
+            export RUST_SRC_PATH=${rustToolchain}/lib/rustlib/src/rust/library
+          '';
+        };
+      });
+}
