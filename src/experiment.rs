97242bd4a57efaa82df67f1bc9518f6ac170cfcc
Jordan McQueen <j@jm.dev>
2025-11-23 21:19:37 +0900

    Pedantic lints for doc formatting

--- src/experiment.rs
+++ src/experiment.rs
@@ -2,22 +2,22 @@ use crate::Simulation;
 use crate::SimulationParameters;
 use rand::Rng;
 
-/// ObjectiveScore is a measure of how a Simulation performed according to an
+/// `ObjectiveScore` is a measure of how a Simulation performed according to an
 /// objective function. This is used to find approximate global optimizations.
 pub type ObjectiveScore = f64;
 
 /// Given a function that generates various configurations of
-/// SimulationParameters, run many simulation replications with varying
-/// SimulationParameters. The parameters are varied by calling the generator.
+/// `SimulationParameters`, run many simulation replications with varying
+/// `SimulationParameters`. The parameters are varied by calling the generator.
 /// The generator may, for example, randomly vary multiple fields of the
 /// parameters. This function tries to approximate the globally optimal
 /// parameters by running the simulation as many times as you specify
-/// (replications_limit), and finds the Simulation that yielded the highest
-/// score from the provided objective_function.
+/// (`replications_limit`), and finds the Simulation that yielded the highest
+/// score from the provided `objective_function`.
 ///
 /// The simplest and most common objective function is to return negative
 /// simulation time. An objective function that returns negative simulation time
-/// will find the Simulation that completed in the least ticks of DiscreteTime.
+/// will find the Simulation that completed in the least ticks of `DiscreteTime`.
 pub fn monte_carlo_experiment(
     mut simulation_parameters_generator: impl FnMut() -> SimulationParameters,
     replications_limit: u32,
