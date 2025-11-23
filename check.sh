23ebec130aefe26dad479118000d5df27279b051
Jordan McQueen <j@jm.dev>
2025-11-23 18:04:20 +0900

    Add consideration for check.sh

--- check.sh
+++ check.sh
@@ -9,7 +9,7 @@ cargo clippy --all-features --all-targets -- -D warnings
 cargo audit
 cargo doc --all-features
 
-licenses_outside_allowlist=$(cargo license | grep -Ev "((MIT)|(Apache-2.0)|(BSD-[23]))")
+licenses_outside_allowlist=$(cargo license | grep -Ev "((MIT)|(Apache-2.0)|(BSD-[23]))") || true;
 if [ -n "$licenses_outside_allowlist" ]; then
     echo -e "\033[31m ERROR: Disallowed licenses detected:\033[0m"
     echo -e "\033[31m$licenses_outside_allowlist\033[0m"
@@ -17,3 +17,8 @@ if [ -n "$licenses_outside_allowlist" ]; then
 fi
 
 RUSTC_BOOTSTRAP=1 cargo udeps
+
+# consider:
+#   https://github.com/cpg314/cargo-workspace-unused-pub
+#   cargo mutants --test-tool=nextest
+#   RUSTDOCFLAGS="--show-coverage" cargo +nightly doc
